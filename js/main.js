60d822a0eb90fb3600b8713e56c143f46835f16b
Nikrosh <nikrosh91@gmail.com>
2021-02-01 19:59:42 +0300

    еще правки

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 
 //Получение случайного целого числа, частично взял с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const randomWholeNumber = (minNumber, maxNumber) => {
+const randomizeWholeNumber = (minNumber, maxNumber) => {
   if (minNumber < 0) {
     minNumber = 0;
-  };
+  }
   if (maxNumber < minNumber) {
     maxNumber = minNumber;
-  };
+  }
   minNumber = Math.ceil(minNumber);
   maxNumber = Math.floor(maxNumber);
   return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-};
+}
 
 //Получение случайного числа с плавающей запятой
 
-const randomNumber = (minNumber, maxNumber, numberAfter) => {
+const randomizeNumber = (minNumber, maxNumber, numberAfter) => {
   if (minNumber < 0) {
     minNumber = 0;
-  };
+  }
   if (maxNumber < minNumber) {
     maxNumber = minNumber;
-  };
+  }
   return Number((Math.random() * (maxNumber - minNumber) + minNumber).toFixed(numberAfter));
-};
+}
